c5d8c10cba5ac948823b57017fbbaef1bf3626d1
bodrich <kostyablood@gmail.com>
2018-08-05 19:21:05 +0500

    Тестовый коммит

--- src/char.cpp
+++ src/char.cpp
@@ -1761,7 +1761,7 @@ const CHAR_DATA::morphs_list_t& CHAR_DATA::get_morphs()
 {
 	return morphs_;
 };
-
+// обрезает строку и выдергивает из нее предтитул
 std::string CHAR_DATA::get_title()
 {
 	std::string tmp = this->player_data.title;
